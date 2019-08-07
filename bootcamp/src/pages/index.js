a56e3cf55babea7a79a50c4f12c384f0693ef6e5
MS <mixa.ru.ru@gmail.com>
2019-08-07 22:02:50 +0300

    Started from Hello World starter template

--- bootcamp/src/pages/index.js
+++ bootcamp/src/pages/index.js
@@ -1,3 +1,12 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+const IndexPage = () => {
+    return (
+        <div>
+            <h1>Hello world!</h1>
+            <h2>I am a Gatsby page</h2>
+        </div>
+    )
+}
+
+export default IndexPage
