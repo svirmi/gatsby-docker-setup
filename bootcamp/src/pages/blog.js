fb5ce948a239e15c68d3959ef3155e9ede1a8292
Mikhail Svirkin <mikhail.svirkin@admiralmarkets.com>
2019-08-08 09:12:28 +0300

    Russian text string

--- bootcamp/src/pages/blog.js
+++ bootcamp/src/pages/blog.js
@@ -5,6 +5,7 @@ const BlogPage = () => {
         <div>
             <h1>Blog page</h1>
             <h2>Posts will come later</h2>
+            <p>И даже на русском языке</p>
         </div>
     )
 }
