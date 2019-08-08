66da22ad9d81d6b9cef39043dcf11362304a51f8
MS <mixa.ru.ru@gmail.com>
2019-08-08 20:43:04 +0300

    links between pages

--- bootcamp/src/pages/contact.js
+++ bootcamp/src/pages/contact.js
@@ -1,10 +1,12 @@
 import React from "react"
+import { Link } from "gatsby";
 
 const ContactPage = () => {
     return (
         <div>
             <h1>Contact page</h1>
             <h2>Contact details</h2>
+            <p><Link to="/">Link to Index page</Link></p>
         </div>
     )
 }
--- bootcamp/src/pages/index.js
+++ bootcamp/src/pages/index.js
@@ -1,10 +1,12 @@
-import React from "react"
+import React from "react";
+import { Link } from "gatsby";
 
 const IndexPage = () => {
     return (
         <div>
             <h1>Hello world!</h1>
             <h2>I am an INDEX Gatsby page</h2>
+            <p><Link to="/contact">Link to Contact page</Link></p>
         </div>
     )
 }
