af730ca50e02c1d378c470bc37ee76d8e1f20208
MS <mixa.ru.ru@gmail.com>
2019-08-25 17:44:14 +0300

    styles module for footer

--- bootcamp/src/components/footer.js
+++ bootcamp/src/components/footer.js
@@ -1,9 +1,12 @@
 import React from "react";
 import { graphql, useStaticQuery } from "gatsby";
 
+import FooterStyles from "./footer.module.scss";
+
+
 const Footer = () => {
 
-    const data = useStaticQuery(graphql`    
+const data = useStaticQuery(graphql`    
         query {
             site {
                 siteMetadata {                    
@@ -14,7 +17,7 @@ const Footer = () => {
     `);
 
     return (
-        <footer>
+        <footer className={FooterStyles.footer}>
             <p>Footer section. © — знак охраны авторского права, представляет собой латинскую литеру C, помещённую в центре окружности.</p>
             <p>{data.site.siteMetadata.email}</p>
         </footer>
